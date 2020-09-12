688fbbc195b1161673b7991ea75fcf5f6392bd80
Kiren Srinivasan <srinitude@gmail.com>
2020-09-12 09:56:39 -0400

    Initial Commit

---
+++ StormViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StormViewer
+//
+//  Created by Kiren Srinivasan on 9/12/20.
+//  Copyright © 2020 Kiren Srinivasan's Practice. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
